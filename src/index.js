c8d0da1dc8e4b0fa407c44c396aabceef5bd603d
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-11-02 16:15:04 +0200

    продовження

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-import { getImages } from './api/apisearch';
-import Notiflix from 'notiflix';
+// import { getImages } from './api/apisearch';
+// import Notiflix from 'notiflix';
 
-const formSubmitRef = document.querySelector('#search-form');
-const galleryRef = document.querySelector('.gallery');
-const loadBtnRef = document.querySelector('.load-more');
+// const formSubmitRef = document.querySelector('#search-form');
+// const galleryRef = document.querySelector('.gallery');
+// const loadBtnRef = document.querySelector('.load-more');
 
-formSubmitRef.addEventListener('submit', onSubmitForm);
-loadBtnRef.addEventListener('click', onClickLoadBtn);
-// обробка події сабміту форми
-function onSubmitForm(e) {
-  e.preventDefault();
-  let searchValue = e.currentTarget.searchQuery.value.trim();
-  if (!searchValue) {
-    Notiflix.Notify.warning(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-    return;
-  }
-}
-
-<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>;
+// formSubmitRef.addEventListener('submit', onSubmitForm);
+// loadBtnRef.addEventListener('click', onClickLoadBtn);
+// // обробка події сабміту форми
+// function onSubmitForm(e) {
+//   e.preventDefault();
+//   let searchValue = e.currentTarget.searchQuery.value.trim();
+//   if (!searchValue) {
+//     Notiflix.Notify.warning(
+//       'Sorry, there are no images matching your search query. Please try again.'
+//     );
+//     return;
+//   }
+//   cteateImageCard(searchValue);
+// }
+ 
+// <div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>;
