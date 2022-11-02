571d52bc4f8dadd9939cf649da347be91ec317a6
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-11-02 18:37:17 +0200

    функція пошуку

--- src/api/apisearch.js
+++ src/api/apisearch.js
@@ -4,21 +4,23 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '31023163-94386fc9f1fd647d996722da2';
 const AMOUNT_PAGE = 40;
 
-const URL = axios.create(BASE_URL, {
-  params: {
-    key: API_KEY,
-    q: '',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    page: 1,
-    per_page: AMOUNT_PAGE,
-  },
-});
-async function getImages(name) {
-  const response = await URL.get(`?q=${name}`);
-  console.log(response.data);
-  return response.data;
+export async function getImages(searchQuery) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: 1,
+        q: searchQuery,
+        per_page: AMOUNT_PAGE,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default getImages;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,51 @@
-// import { getImages } from './api/apisearch';
-// import Notiflix from 'notiflix';
+import { getImages } from './api/apisearch';
+import Notiflix from 'notiflix';
 
-// const formSubmitRef = document.querySelector('#search-form');
-// const galleryRef = document.querySelector('.gallery');
-// const loadBtnRef = document.querySelector('.load-more');
+const formSubmitRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+const loadBtnRef = document.querySelector('.load-more');
 
-// formSubmitRef.addEventListener('submit', onSubmitForm);
+formSubmitRef.addEventListener('submit', onSubmitForm);
 // loadBtnRef.addEventListener('click', onClickLoadBtn);
-// // обробка події сабміту форми
-// function onSubmitForm(e) {
-//   e.preventDefault();
-//   let searchValue = e.currentTarget.searchQuery.value.trim();
-//   if (!searchValue) {
-//     Notiflix.Notify.warning(
-//       'Sorry, there are no images matching your search query. Please try again.'
-//     );
-//     return;
-//   }
-//   cteateImageCard(searchValue);
-// }
- 
-// <div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>;
+// обробка події сабміту форми
+async function onSubmitForm(e) {
+  e.preventDefault();
+  let searchQuery = e.currentTarget.searchQuery.value.trim();
+  if (!searchQuery) {
+    Notiflix.Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  cteateImageCard(searchQuery);
+}
+async function cteateImageCard(name) {
+    try {
+        const data = await getImages(name);
+        const arrData = data.hits;
+        if (arrData.length === 0) {
+            Notiflix.Notify.warning(
+                'Sorry, there are no images matching your search query. Please try again.'
+            );
+            return;
+        }
+    }
+
+
+        /* <div class="photo-card">
+        <img src="" alt="" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+          </p>
+        </div>
+      </div>; */
+    
